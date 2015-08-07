0138455bb5a2518ace2618a4405c87145e7c3207
Jordan Moncharmont <jormon@gmail.com>
2015-08-06 19:54:50 -0700

    rails 4.2.0 compatible

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -4,6 +4,6 @@ Spree::Product.class_eval do
   has_and_belongs_to_many :subscription_intervals, :foreign_key => "product_id", :join_table => 'spree_subscription_intervals_products'
   alias :intervals :subscription_intervals
 
-  scope :subscribable, where(:subscribable => true)
-  scope :unsubscribable, where(:subscribable => false)
+  scope :subscribable, -> { where subscribable: true }
+  scope :unsubscribable, -> { where subscribable: false }
 end
