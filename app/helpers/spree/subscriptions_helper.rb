ec08fc39807459ade8ad17b3167d1a11fe74f8ed
Jordan Moncharmont <jormon@gmail.com>
2015-12-03 14:40:49 -0800

    fix a variant/product mixup

--- app/helpers/spree/subscriptions_helper.rb
+++ app/helpers/spree/subscriptions_helper.rb
@@ -3,13 +3,13 @@ module Spree::SubscriptionsHelper
     Spree::Money.new(subscription.line_items.map(&:amount).sum).to_html
   end
 
-  def subscription_price_difference_for(product:)
-    Spree::Money.new product.price - product.subscribed_price
+  def subscription_price_difference_for(variant:)
+    Spree::Money.new variant.price - variant.subscribed_price
   end
 
   def subscription_price_difference_for_line_item(line_item)
     difference = subscription_price_difference_for \
-      product: line_item.product
+      variant: line_item.variant
     Spree::Money.new(difference.money.to_f * line_item.quantity)
   end
 end
