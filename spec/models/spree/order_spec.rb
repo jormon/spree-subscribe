9d77dc3a1c7ea2b174a529919414544849efb174
Jordan Moncharmont <jormon@gmail.com>
2016-08-29 18:51:09 -0700

    Add #subscription? method to order
    Returns true if a recurrence or originator of subscriptions
    
    https://app.asana.com/0/221747295457/163963330385549

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -5,4 +5,25 @@ describe Spree::Order do
     it "it removes un-referenced subscriptions"
     it "keeps referenced subscriptions"
   end
+
+  describe "#subscription?" do
+    before :each do
+      allow(subject).to receive(:subscription_id).and_return nil
+      allow(subject).to receive(:subscriptions).and_return []
+    end
+    it "returns false" do
+      expect(subject).to_not be_subscription
+    end
+
+    it "returns true if an order is a recurrance of a subscription" do
+      allow(subject).to receive(:subscription_id).and_return 123
+      expect(subject).to be_subscription
+    end
+
+    it "returns true if an order originates subscriptions" do
+      subscription = double "subscription"
+      allow(subject).to receive(:subscriptions).and_return [subscription]
+      expect(subject).to be_subscription
+    end
+  end
 end
